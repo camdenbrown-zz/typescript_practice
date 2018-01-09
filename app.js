d63496efd97f76214f4e1c456be87c26c9c2dc0f
Camden Brown <camden.brown@metova.com>
2018-01-09 11:19:39 -0600

    init

---
+++ app.js
@@ -0,0 +1,6 @@
+var myName = 'Max';
+var myAge = 27;
+var hasHobbies = true;
+var myRealAge;
+myRealAge = 40;
+myRealAge = 'Camden';
---
+++ app.ts
@@ -0,0 +1,43 @@
+// String
+let myName = 'Max';
+
+// Number
+let myAge = 27;
+
+// Boolean
+let hasHobbies = true;
+
+// Assign Types
+let myRealAge: number;
+myRealAge = 40;
+myRealAge = 'Camden'; //error
+
+
+// Array
+let hobbies = ["Cooking", "Sports"];
+hobbies = [100]; //error
+
+let hobbies2: any[] = ["Cooking", "Sports"];
+hobbies2  = [100];
+
+// Tuples
+let address: [string, number] = ["Superstreet", 99];
+
+// Enumss
+enum Color {
+  Gray, // 0
+  Green = 100,
+  Blue // 101
+}
+let myColor: Color = Color.Green;
+
+// functions
+function returnMyName(): string {
+  return 'Camden';
+}
+
+//void
+function sayHello(): void {
+  console.log('HELLO');
+}
+
