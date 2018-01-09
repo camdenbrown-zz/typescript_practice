a50b50714d87575254774fccdc4597aecd40029f
Camden Brown <camden.brown@metova.com>
2018-01-09 11:31:56 -0600

    object types

--- app.ts
+++ app.ts
@@ -18,28 +18,40 @@ let hobbies = ["Cooking", "Sports"];
 hobbies = [100]; //error
 
 let hobbies2: any[] = ["Cooking", "Sports"];
-hobbies2  = [100];
+hobbies2 = [100];
 
 // Tuples
 let address: [string, number] = ["Superstreet", 99];
 
 // Enumss
 enum Color {
-  Gray, // 0
-  Green = 100,
-  Blue // 101
+    Gray, // 0
+    Green = 100,
+    Blue // 101
 }
+
 let myColor: Color = Color.Green;
 
 // functions
 function returnMyName(): string {
-  return 'Camden';
+    return 'Camden';
 }
 
 //void
 function sayHello(): void {
-  console.log('HELLO');
+    console.log('HELLO');
 }
 
 //function types
 let multiply: (firstNumber: number, secondNumber: number) => number;
+
+//objects
+let userData: { name: string, age: number } = {
+    name: 'Camden',
+    age: 26
+};
+
+userData = {
+    name: 'Hello',
+    age: 22
+};
